2aa4fdedeb26c439636c7ad3d32213d87c4da6d2
Cody Mikol <cody.mikol@boltstech.com>
2024-01-15 11:36:32 -0500

    feat: move init into multiverse
    hoping this makes lazyvim recognize it
    
    Fixes N/A

--- lua/multiverse/init.lua
+++ lua/multiverse/init.lua
@@ -1,6 +1,7 @@
 local M = {}
 
-M.setup = function()
+M.setup = function(opts)
+	opts = opts or {} -- todo: do I need this for LazyVim to recognize this?
 	local WorkspacesIntegration = require("integrations.workspaces")
 	WorkspacesIntegration.registerHooks()
 end
