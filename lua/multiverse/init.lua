3c82bd321b3094d112914e713a9b9a94190b6133
Cody Mikol <cody.mikol@boltstech.com>
2024-01-15 11:38:19 -0500

    feat: add debug logging to test if my plugin is loaded...
    ...
    
    Fixes N/A

--- lua/multiverse/init.lua
+++ lua/multiverse/init.lua
@@ -1,6 +1,8 @@
 local M = {}
 
 M.setup = function(opts)
+	vim.notify("Setting up Multiverse")
+
 	opts = opts or {} -- todo: do I need this for LazyVim to recognize this?
 	local WorkspacesIntegration = require("integrations.workspaces")
 	WorkspacesIntegration.registerHooks()
