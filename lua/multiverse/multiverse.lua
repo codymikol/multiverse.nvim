db682beffa9422347fc4cc82e5ab8d73144e470b
Cody Mikol <cody.mikol@boltstech.com>
2024-01-15 21:46:35 -0500

    feat: moving MAIN to multiverse.lua
    this will hopefully allow lazy to find the setup
    function...
    
    Fixes N/A

--- lua/multiverse/multiverse.lua
+++ lua/multiverse/multiverse.lua
@@ -49,4 +49,12 @@ M.hydrate = function()
 	Windows.hydrate()
 end
 
+M.setup = function(opts)
+	vim.notify("Setting up Multiverse")
+
+	opts = opts or {} -- todo: do I need this for LazyVim to recognize this?
+	local WorkspacesIntegration = require("integrations.workspaces")
+	WorkspacesIntegration.registerHooks()
+end
+
 return M
