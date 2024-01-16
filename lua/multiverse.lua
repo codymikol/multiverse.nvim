e41f00a935073fb8a6234d7ffafcbb97ffa7aacc
Cody Mikol <cody.mikol@boltstech.com>
2024-01-15 22:18:20 -0500

    chore: clean up multiverse.lua
    ...
    
    Fixes N/A

--- lua/multiverse.lua
+++ lua/multiverse.lua
@@ -2,7 +2,6 @@ local Workspaces = require("integrations.workspaces")
 local Multiverse = {}
 
 Multiverse.setup = function(config)
-	config = config or {} -- todo: do I need this for LazyVim to recognize this?
 	Workspaces.registerHooks()
 end
 
