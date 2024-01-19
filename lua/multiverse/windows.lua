0af36d7ebcc99d631cb91d92fcdf26fb440af60b
Cody Mikol <cody.mikol@boltstech.com>
2024-01-18 23:29:26 -0500

    chore: remove debug logs that are useless
    ...
    
    Fixes N/A

--- lua/multiverse/windows.lua
+++ lua/multiverse/windows.lua
@@ -6,16 +6,16 @@ local M = {}
 -- This will find all open windows and persist their positioning
 -- and buffer contents into a file which will be used for rehydration.
 M.closeAll = function()
-	log("TODO: close all open windows")
+	-- TODO: close all open windows
 end
 
 -- This will close all open windows.
 M.saveAll = function()
-	log("TODO: save current open windows")
+	-- TODO: save current open windows
 end
 
 M.hydrate = function()
-	log("TODO: load saved windows")
+	-- TODO: load saved windows
 end
 
 return M
