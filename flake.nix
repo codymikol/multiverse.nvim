605504b16405fe79d6e303a104f76682c86c179f
Cody Mikol <hi@codymikol.com>
2026-08-07 07:29:43 -0400

    feat: add spindrift
    ...
    
    Fixes N/A

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  description = "multiverse.nvim: a project management plugin for neovim";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    spindrift.url = "github:jordansmall/spindrift";
+  };
+
+  outputs = inputs@{ 
+    self,
+    nixpkgs,
+    flake-utils,
+    flake-parts,
+    spindrift,
+    }:
+
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+
+      imports = [ spindrift.flakeModules.default ];
+
+      perSystem =
+        { config, pkgs, ... }:
+        {
+          spindrift = {
+
+            infra.image = {
+              packages = p: [ p.neovim ];
+              prefetch = "nvim --headless '+Lazy! sync' +qa";
+            };
+
+            forge = {
+              backend = "github";
+              repoSlug = "codymikol/multiverse.nvim";
+            };
+
+            git.user = {
+              name = "bot";
+              email = "hi@codymikol.com";
+            };
+          };
+
+          devShells.default = pkgs.mkShell {
+            packages = [ config.packages.spindrift ];
+          };
+
+        };
+    };
+}
+
+
+
+
