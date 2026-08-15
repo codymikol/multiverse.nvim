7880bf0a0d30fadb7353d309c2287412b6470084
bot <hi@codymikol.com>
2026-08-15 11:42:01 +0000

    refactor(cleanup_manager): remove dead unreachable functions
    close_terminal_buffer and close_editable_buffer were declared
    without `local`, leaking as globals into the Neovim runtime. Neither
    is called anywhere nor exposed on M, so require() callers can't
    reach them either. Their logic is fully superseded by
    buffer_manager.M.closeAll, which cleanup() reaches via
    buffer_manager.closeAllBuffers() and which already branches on
    terminal vs. non-terminal buftype.
    
    Closes #135

--- lua/multiverse/managers/cleanup_manager.lua
+++ lua/multiverse/managers/cleanup_manager.lua
@@ -1,28 +1,8 @@
-local log = require("multiverse.log")
 local buffer_manager = require("multiverse.managers.buffer_manager")
 
 local M = {}
 
-function close_terminal_buffer(buffer)
-	local status, err = pcall(vim.api.nvim_buf_delete, buffer, { force = true })
-	if status then
-		log.info("successfully deleted terminal buffer " .. buffer)
-	else
-		log.info("error deleting buffer " .. buffer .. ", error: " .. vim.inspect(err))
-	end
-end
-
-function close_editable_buffer(buffer)
-	local status, err = pcall(vim.api.nvim_buf_delete, buffer, {})
-	if status then
-		log.info("successfully closed buffer " .. buffer)
-	else
-		log.info("error deleting buffer " .. buffer .. ", error: " .. err)
-	end
-end
-
 M.cleanup = function()
-	-- the default behavior is to close windows when they have no buffer anymore...
 	buffer_manager.closeAllBuffers()
 end
 
