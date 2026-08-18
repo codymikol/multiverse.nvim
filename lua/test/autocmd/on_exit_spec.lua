85c60e92e4eceff82ca57816e039cb0fc98bc2f5
bot <hi@codymikol.com>
2026-08-18 04:13:47 +0000

    refactor(test): tighten on_exit spec per review
    Scope nvim_exec_autocmds calls to the multiverse_on_exit group so the
    tests don't fire every QuitPre autocmd in the process, hoist the
    repeated augroup name into a local, stub multiverse_manager.save in
    the register describe block to avoid leaving a live QuitPre autocmd
    bound to real save() I/O, and assert the surviving autocmd is
    actually the QuitPre one this module registers.

--- lua/test/autocmd/on_exit_spec.lua
+++ lua/test/autocmd/on_exit_spec.lua
@@ -2,19 +2,29 @@ local multiverse_manager = require("multiverse.managers.multiverse_manager")
 local on_exit = require("multiverse.autocmd.on_exit")
 local stub = require("luassert.stub")
 
+local augroup_name = "multiverse_on_exit"
+
 describe("on_exit", function()
 	describe("register", function()
+		local save_stub
+
+		before_each(function()
+			save_stub = stub(multiverse_manager, "save")
+		end)
+
 		after_each(function()
-			pcall(vim.api.nvim_del_augroup_by_name, "multiverse_on_exit")
+			save_stub:revert()
+			pcall(vim.api.nvim_del_augroup_by_name, augroup_name)
 		end)
 
 		it("should only register a single QuitPre autocmd when called multiple times", function()
 			on_exit.register()
 			on_exit.register()
 
-			local autocmds = vim.api.nvim_get_autocmds({ group = "multiverse_on_exit", event = "QuitPre" })
+			local autocmds = vim.api.nvim_get_autocmds({ group = augroup_name, event = "QuitPre" })
 
 			assert.equals(1, #autocmds)
+			assert.equals("QuitPre", autocmds[1].event)
 		end)
 	end)
 
@@ -27,13 +37,13 @@ describe("on_exit", function()
 
 		after_each(function()
 			save_stub:revert()
-			pcall(vim.api.nvim_del_augroup_by_name, "multiverse_on_exit")
+			pcall(vim.api.nvim_del_augroup_by_name, augroup_name)
 		end)
 
 		it("should invoke multiverse_manager.save() exactly once", function()
 			on_exit.register()
 
-			vim.api.nvim_exec_autocmds("QuitPre", {})
+			vim.api.nvim_exec_autocmds("QuitPre", { group = augroup_name })
 
 			assert.stub(save_stub).was.called(1)
 		end)
@@ -42,7 +52,7 @@ describe("on_exit", function()
 			on_exit.register()
 			on_exit.register()
 
-			vim.api.nvim_exec_autocmds("QuitPre", {})
+			vim.api.nvim_exec_autocmds("QuitPre", { group = augroup_name })
 
 			assert.stub(save_stub).was.called(1)
 		end)
