b35ae4381a1117b49cb5f99a6314d59296921fc7
bot <hi@codymikol.com>
2026-09-10 01:38:23 +0000

    fix(hydration): contain pipeline errors, guarantee cleanup
    Wraps M.hydrate's stage calls in pcall so a thrown error (e.g. a
    missing integration plugin) surfaces via vim.notify/log.error
    instead of aborting the pipeline, and moves the scratch-buffer
    cleanup stage outside the guarded block so it always runs.

--- lua/multiverse/managers/hydration_manager.lua
+++ lua/multiverse/managers/hydration_manager.lua
@@ -3,6 +3,7 @@ local buffer_manager = require("multiverse.managers.buffer_manager")
 local neotree_integration = require("integrations.neotree")
 local tabpage_manager = require("multiverse.managers.tabpage_manager")
 local window_layout_manager = require("multiverse.managers.window_layout_manager")
+local log = require("multiverse.log")
 local M = {}
 
 ---@param universe Universe
@@ -20,21 +21,28 @@ M.hydrate = function(selected_universe)
 		return
 	end
 
-	setCwd(universe)
+	local success, hydrate_err = pcall(function()
+		setCwd(universe)
 
-	buffer_manager.hydrateBuffersForUniverse(universe)
+		buffer_manager.hydrateBuffersForUniverse(universe)
 
-	tabpage_manager.hydrate(universe)
+		tabpage_manager.hydrate(universe)
 
-	window_layout_manager.hydrate(universe)
+		window_layout_manager.hydrate(universe)
 
-	neotree_integration.hydrate()
+		neotree_integration.hydrate()
+	end)
 
-  buffer_manager.close_generated_nofile_scratch_buffers()
+	if not success then
+		local message = "Error hydrating universe: " .. vim.inspect(hydrate_err)
+		log.error("%s", message)
+		vim.notify(message, vim.log.levels.ERROR)
+	end
 
+	buffer_manager.close_generated_nofile_scratch_buffers()
 end
 
 return M
 
 -- todo(mikol): Add an adapter layer that allows developers who wish to add support for external plugins to create adapters and have a configuration that allows you to enable/disable them.
--- todo(mikol): before v1.0.0, we should check to see if these plugins have been loaded before trying to hydrate them. :D realted ^^
+-- todo(mikol): before v1.0.0, we should check to see if these plugins have been loaded before trying to hydrate them. :D realted ^^ (done for neotree, other integrations still unguarded)
